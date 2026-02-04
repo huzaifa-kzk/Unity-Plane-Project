e1be3d14c4ae12709bfbcd386098a6b5f444d37d
huzaif-kzk <huzaifakazim71@gmail.com>
2026-02-04 20:52:03 +0500

    added declaration for app

---
+++ plane-controller-app/declaration.d.ts
@@ -0,0 +1,20 @@
+// plane-controller-app/declarations.d.ts
+declare module '*.png' {
+  const content: number;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: number;
+  export default content;
+}
+
+declare module '*.ttf' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.otf' {
+  const content: string;
+  export default content;
+}
